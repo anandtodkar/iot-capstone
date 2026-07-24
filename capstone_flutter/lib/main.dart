2538d6404821282b288c565dae4f0def61d7d09c
anandtodkar <54904452+anandtodkar@users.noreply.github.com>
2026-07-24 18:05:46 -0400

    Remove hardcoded AWS S3 credentials
    Removed hardcoded AWS S3 access and secret keys.

--- capstone_flutter/lib/main.dart
+++ capstone_flutter/lib/main.dart
@@ -230,8 +230,8 @@ class _HomeScreenState extends State<HomeScreen> {
           region: region,
           host: "s3.$region.amazonaws.com",
           bucketId: bucketId,
-          accessKey: "AKIAI3AI7EVZFZE61234",
-          secretKey: "+X4L+EOs0+XZnMLt4IFKgLx71IoPKPLIJK7O1234");
+          accessKey: "",
+          secretKey: "");
 
       final response = await s3client.getObject(key);
       setState(() {
